eaf92d3bfb3fbf7f89af0a939753783f7c9b1bfd
ray <ray@rvoelke-laptop.plch.net>
2018-12-12 21:31:39 -0500

    end of day 12!

--- day12.py
+++ day12.py
@@ -4,64 +4,64 @@ from collections import deque
 import sys
 sys.stdout.flush()
 
-# initial_state = "#........#.#.#...###..###..###.#..#....###.###.#.#...####..##..##.#####..##...#.#.....#...###.#.####"
-# input = """
-# #..## => .
-# ##..# => #
-# ..##. => .
-# .##.# => #
-# ..... => .
-# ..### => #
-# ###.# => #
-# #.... => .
-# #.##. => #
-# .#.## => #
-# #...# => .
-# ...## => .
-# ###.. => #
-# .#..# => .
-# ####. => .
-# ....# => .
-# ##### => #
-# .###. => .
-# #..#. => .
-# ##... => #
-# .#... => #
-# #.#.# => .
-# ..#.. => #
-# ...#. => #
-# ##.#. => .
-# .##.. => #
-# .#.#. => .
-# #.#.. => .
-# ..#.# => #
-# #.### => .
-# ##.## => .
-# .#### => #
-# """
+initial_state = "#........#.#.#...###..###..###.#..#....###.###.#.#...####..##..##.#####..##...#.#.....#...###.#.####"
+input = """
+#..## => .
+##..# => #
+..##. => .
+.##.# => #
+..... => .
+..### => #
+###.# => #
+#.... => .
+#.##. => #
+.#.## => #
+#...# => .
+...## => .
+###.. => #
+.#..# => .
+####. => .
+....# => .
+##### => #
+.###. => .
+#..#. => .
+##... => #
+.#... => #
+#.#.# => .
+..#.. => #
+...#. => #
+##.#. => .
+.##.. => #
+.#.#. => .
+#.#.. => .
+..#.# => #
+#.### => .
+##.## => .
+.#### => #
+"""
 
 # example ...
-initial_state = "#..#.#..##......###...###"
-input = """
-...## => #
-..#.. => #
-.#... => #
-.#.#. => #
-.#.## => #
-.##.. => #
-.#### => #
-#.#.# => #
-#.### => #
-##.#. => #
-##.## => #
-###.. => #
-###.# => #
-####. => #
-"""
+# initial_state = "#..#.#..##......###...###"
+# input = """
+# ...## => #
+# ..#.. => #
+# .#... => #
+# .#.#. => #
+# .#.## => #
+# .##.. => #
+# .#### => #
+# #.#.# => #
+# #.### => #
+# ##.#. => #
+# ##.## => #
+# ###.. => #
+# ###.# => #
+# ####. => #
+# """
 
 # turn the initial_state string into a list of characters
 pots = deque(initial_state)
-print(pots)
+# print(pots)
 
 rule = dict({
     'test': '',
@@ -69,10 +69,6 @@ rule = dict({
 })
 
 rules = list()
-
-# count removed from front list
-cl = list()
-cr = list()
 
 # create the rules list...
 input = input.strip().split('\n')
@@ -82,7 +78,7 @@ for item in input:
     rules.append(rule.copy())
     rules[-1]['test'] = temp[1]
     rules[-1]['result'] = temp[2]
-print(rules)
+# print(rules)
 
 # grow the array so that we have 4 empty pots on each end ...
 def grow_pots():
@@ -95,6 +91,8 @@ def grow_pots():
     pots.append('.')
     pots.append('.')
 
+    return 4
+
 
 def trim_pots():
     # first trim from the left ...
@@ -102,21 +100,34 @@ def trim_pots():
     count_removed_left = 0
     while pots[0] == '.':
         pots.popleft()
-        # count_removed_left +=1
+        count_removed_left +=1
         # print('removed: {}'.format(pot))
 
-    count_removed_right = 0
+    # count_removed_right = 0
     while pots[-1] == '.':
         pots.pop()
         # count_removed_right +=1
 
-    # return count_removed_left, count_removed_right
+    return count_removed_left
 
 # pots at gen 0
 print('starting length: {}'.format)
 print(''.join(pots))
 
-gen_range = 20
+add_left = 0
+del_left = 0
+
+gen_range = 100
+
+# gen_range = 100   start index count at: 18    8866
+# gen_range = 101   start index count at: 19    8946
+# gen_range = 101                               9026
+# every gen causes the count to go up by 80
+
+# 5000 = 400866 start index count at: 4918
+# 10000 = 800866 start index count at: 9918
+# 15000 = 1200866 start index count at: 14918
+
 for gen in range(gen_range):
 
     if gen % 5000 == 0:
@@ -124,7 +135,7 @@ for gen in range(gen_range):
         print(''.join(pots))
         sys.stdout.flush()
 
-    grow_pots()
+    add_left += grow_pots()
 
     # pot to store our next gen in ...
     temp_pots = pots.copy()
@@ -150,7 +161,7 @@ for gen in range(gen_range):
                 # print('match ... {} == {}'.format(sample, rule['test']))
                 temp_pots[i] = rule['result']
                 rule_count += 1
-                # break
+                break
 
         # print('matched rules: {}'.format(rule_count))
         if rule_count == 0:
@@ -158,23 +169,18 @@ for gen in range(gen_range):
         
     # replace the temp copy with the current version
     pots = temp_pots.copy()
-    trim_pots()
+    del_left += trim_pots()
+
     # cl.append(count_removed_left)
     # cr.append(count_removed_right)
 
     # print(''.join(pots))
 
 
+print('add_left: {}'.format(add_left))
+print('del_left: {}'.format(del_left))
 
-# print('min removed left: {}'.format(min(cl)))
-# print('max removed left: {}'.format(max(cl)))
-
-# print('min removed right: {}'.format(min(cr)))
-# print('max removed right: {}'.format(max(cr)))
-
-start_count = -2
-# start_count = max(cr) - max(cl)
-
+start_count = del_left - add_left
 print("start index count at: {}".format(start_count))
 
 # 3159 at start index count = -2 .. not right ...
@@ -188,3 +194,7 @@ for i in range(len(pots)):
 print(''.join(pots))
 
 print('sum index of pots with plants: {}'.format(count))
+
+gen_fifty_billion = ((50000000000 - gen_range) * 80) + count
+
+print('gen_fifty_billion: {}'.format(gen_fifty_billion))
